afbd4043770d8df560ed8a3d0ab19830c9937229
afolabiibrahimtech <afolabiibrahim.dream0@gmail.com>
2026-09-14 15:07:48 -0700

    new class activity

---
+++ PersonProject/PersonProject/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonProject
+{
+    class Person //internal class
+    {
+        //state vriables
+       public string Name;
+       public int Age;
+       public string Profession;
+
+        //behaviors
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Profession: {Profession}");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.Name = "Ibrahim Afolabi";
+            p.Age = 29;
+            p.Profession = "Teacher";
+
+            p.Display();
+        }
+    }
+}
---
+++ csproject/csproject/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSProject
+{
+    class My
+    {
+        private int _x;
+        int _y;
+        int _sum;
+        public void Input()
+        {
+            Console.WriteLine("Enter the first number");
+            _x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            _y = int.Parse(Console.ReadLine());
+        }
+    }
+    class test
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x+"+"+y+"="+(x+y));  //string concatenated output
+            Console.WriteLine("{0}+{1}={2}", x, y, (x + y)); //Parametric output
+            Console.WriteLine($"{x}+{y}={x + y}");  //c# output this is readable and easier
+
+           // MyClass obj =new MyClass();
+        }
+    }
+}
