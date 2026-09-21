8a6c1c7b4c4ebb50905c2151d31191449081f4ea
afolabiibrahimtech <afolabiibrahim.dream0@gmail.com>
2026-09-21 11:22:55 -0700

    BankAccountProject and linear array project

---
+++ BankAccountProject/BankAccountProject/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace BankAccountProject
+{
+    class BankAccount
+    {
+        //state variable/fileds
+        public string Name;
+        public int AccountID;
+        public double Balance;
+
+
+
+    
+    
+        //behaviours/ methods  
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+                Balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+                return true;
+            }
+            Console.WriteLine("Withdrawal Unsuccessful");
+            return false;
+        }
+        public bool transfer(double amount, BankAccount receiver)
+        {
+            if (Withdraw(amount))
+            {
+                receiver.Deposit(amount);
+                return true;
+            }
+            return false;
+        }
+
+        public void Accountinfo()
+        {
+            Console.WriteLine($"Name: { Name }, Account ID: { AccountID}, Balance: { Balance}");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Account ID: {AccountID}, Balance: {Balance}";
+        }
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount david = new BankAccount { Name = "David Alwright", AccountID = 1111 };
+            david.Deposit(1000);
+
+            BankAccount susan = new BankAccount { Name = "Susan Harper", AccountID = 2222 };
+            susan.Deposit(50000);
+
+            Console.WriteLine(david);
+            Console.WriteLine(susan);
+
+            Console.WriteLine($"Making a deposit of $1000 to {david.Name}'s Account");
+            david.Deposit(1000);
+            Console.WriteLine($"Making a withdrawal of $10000 to {susan.Name}'s Account");
+            susan.Withdraw(10000);
+
+            Console.WriteLine($"Making a transfer of $1000 from {susan.Name}'s to {david.Name}'s Account");
+            susan.transfer(1000, david);
+
+
+            Console.WriteLine(david);
+            Console.WriteLine(susan);
+
+
+        }
+    }
+}
---
+++ LinearArrayProject/LinearArrayProject/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace LinearArrayProject
+{
+    internal class Program
+    {
+        static void DisplayLinearArray(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void RandomPopulate(int[] x)
+        {
+            Random r = new Random();
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = r.Next(1, 100); //in range 1..99
+            }
+        }
+
+        static int isFound(int[] x, int sVal)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == sVal)
+                    return i; //in range 1..99
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] x;
+            Console.WriteLine("Enter the array size: ");
+            int sz = int.Parse(Console.ReadLine());
+            x = new int[sz];
+            RandomPopulate(x);
+            DisplayLinearArray(x);
+
+            Console.WriteLine("Enter a search value: ");
+            int sVal = int.Parse(Console.ReadLine());
+
+            int index = isFound(x, sVal);
+
+            if (index >= 0)
+            {
+                Console.WriteLine($"The search value {sVal} is found at index: {index}");
+            }
+            else
+            {
+                Console.WriteLine("The search value is not found!");
+            }
+
+            Console.WriteLine($"Length of the array: {x.Length}");
+            Console.WriteLine($"Total dimension of the array: {x.Rank}");
+
+            Console.WriteLine("Performing sorting operation...");
+            Array.Sort(x);
+            Console.WriteLine("Sorted Array Elements: ");
+            DisplayLinearArray(x);
+
+            Console.WriteLine("Sorted Array Elements (Descending order): ");
+            Array.Reverse(x);
+            DisplayLinearArray(x);
+
+        }
+    }
+}
